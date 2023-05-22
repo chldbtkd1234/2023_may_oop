d31181e37f847ae2602d108b234a10d94c389587
yeongung choe <133702062+chldbtkd1234@users.noreply.github.com>
2023-05-22 19:23:31 +0900

    Add files via upload

---
+++ week2/Assignment1.java
@@ -0,0 +1,20 @@
+package week2;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		int choe = 90;
+		int yeong = 80;
+		int ung = 100;
+		int choe1 = 270;
+		int yeong1 = 90;
+		
+		System.out.println("kor" + " " + choe );
+		System.out.println("mat" + " " + yeong );
+		System.out.println("eng" + " " + ung );
+		System.out.println("sum" + " " + choe1 );
+		System.out.println("avg" + " " + yeong1 );
+
+	}
+
+}
---
+++ week2/Assignment3.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+        int  x, y, z ;
+        
+        x= 10;
+        y= 20;
+        z= x + y;
+         
+		
+		System.out.println(x + " + " + y + " = " + (x + y) );
+
+	}
+
+}
