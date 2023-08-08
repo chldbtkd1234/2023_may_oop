39bb15cdff751fdecd1992e3d36a6e3dfbfe4aa4
yeongung choe <133702062+chldbtkd1234@users.noreply.github.com>
2023-08-08 13:23:24 +0900

    Add files via upload

---
+++ 9_baek/_04.java
@@ -0,0 +1,27 @@
+package _09_baek;
+
+import java.util.Scanner;
+
+public class _04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {//
+            int K = sc.nextInt();
+            boolean Sosu = true;
+            if(K==1){
+                continue;
+            }
+            for(int j=2; j<K; j++) { 
+                if (K % j == 0) {
+                    Sosu = false;
+                }
+            }
+                if(Sosu) {
+                    count++;
+                }
+        }
+        System.out.println(count);
+    }
+}
