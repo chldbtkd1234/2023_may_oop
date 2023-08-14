a508173fc59220743f5a4cf7bfae3c677473eab9
yeongung choe <133702062+chldbtkd1234@users.noreply.github.com>
2023-08-14 22:24:27 +0900

    Add files via upload

---
+++ 5_String/_04.java
@@ -0,0 +1,14 @@
+package _05_String;
+
+import java.util.Scanner;
+
+public class _04 {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int ch = in.next().charAt(0);
+        
+		System.out.print(ch);
+	}
+}
